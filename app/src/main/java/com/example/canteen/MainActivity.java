31cee3ccc6146c4b0522d18d9130443dbe613c0f
PHATTT <trantanphat125678@gmail.com>
2023-02-16 22:47:16 +0700

    first commit

--- app/src/main/java/com/example/canteen/MainActivity.java
+++ app/src/main/java/com/example/canteen/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.canteen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.pager);
+
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.setTabGravity(tabLayout.GRAVITY_FILL);
+
+        final PaperAdapter paperAdapter = new PaperAdapter(getSupportFragmentManager(), this, tabLayout.getTabCount());
+        viewPager.setAdapter(paperAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
     }
 }
